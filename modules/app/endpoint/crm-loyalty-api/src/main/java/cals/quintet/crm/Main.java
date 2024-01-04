4caf707f0cbd324cbae24f819f8d42a1b4a22be4
sy.cha <sy.cha@quintet.co.kr>
2024-01-04 18:02:37 +0900

    Initial commit

---
+++ modules/app/endpoint/crm-loyalty-api/src/main/java/cals/quintet/crm/Main.java
@@ -0,0 +1,11 @@
+package cals.quintet.crm;
+
+/**
+ * @author ${USER}
+ * @since 1.0
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ modules/app/endpoint/crm-membership-api/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author ${USER}
+ * @since 1.0
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+    }
+}
