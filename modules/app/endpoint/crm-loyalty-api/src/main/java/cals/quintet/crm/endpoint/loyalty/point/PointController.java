67868996a5fc2419966cbc6fcd69ecb4835a7207
sy.cha <sy.cha@quintet.co.kr>
2024-01-08 15:04:15 +0900

    buildSrc 적용 완료

---
+++ modules/app/endpoint/crm-loyalty-api/src/main/java/cals/quintet/crm/endpoint/loyalty/point/PointController.java
@@ -0,0 +1,17 @@
+package cals.quintet.crm.endpoint.loyalty.point;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sycha
+ * @since 1.0
+ */
+@RestController
+public class PointController {
+
+    @GetMapping("/")
+    public String test(){
+        return "안뇽";
+    }
+}
