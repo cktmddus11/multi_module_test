f8b1b0886d9aac3b99f0a0427602b56df1470275
sy.cha <sy.cha@quintet.co.kr>
2024-01-16 13:15:05 +0900

    포인트 서비스 로직 연결

--- modules/app/endpoint/crm-loyalty-api/src/main/java/cals/quintet/crm/endpoint/loyalty/point/PointApiController.java
+++ modules/app/endpoint/crm-loyalty-api/src/main/java/cals/quintet/crm/endpoint/loyalty/point/PointApiController.java
@@ -3,19 +3,21 @@ package cals.quintet.crm.endpoint.loyalty.point;
 import cals.quintet.crm.business.approval.point.PointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sycha
  * @since 1.0
  */
+@RequestMapping("/")
 @RestController
 public class PointApiController {
 
     @Autowired
     private PointService pointService;
 
-    @GetMapping("/")
+    @GetMapping("/point")
     public String test(){
         String accrual = pointService.accrual();
         return accrual;
